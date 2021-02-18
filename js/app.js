49851ffc4adea6392edcaa1a94d36289caef65a0
ahnanne <{ahnanne93@gmail.com}>
2021-02-18 15:39:55 +0900

    feat: Complete style

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
-console.log('Hi');
+const carousel = ($container, images) => {
+  const $img = document.createElement('img');
+};
+
+carousel(document.querySelector('.carousel'), [
+  './css/img/1.jpg',
+  './css/img/2.jpg',
+  './css/img/3.jpg',
+  './css/img/4.jpg',
+  './css/img/5.jpg',
+  './css/img/6.jpg',
+  './css/img/7.jpg',
+  './css/img/8.jpg',
+  './css/img/9.jpg',
+  './css/img/10.jpg',
+  './css/img/11.jpg',
+]);
+
+/*
+<li class="list-item">
+            <figure>
+              <img src="./css/img/1.jpg" alt="1" width="160px">
+              <figcaption>
+                <em class="title">Britta Clay Art</em>
+                <span class="date">Sat, 24 October 2020</span>
+              </figcaption>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure>
+              <img src="./css/img/2.jpg" alt="1" width="160px">
+              <figcaption>
+                <em class="title">Britta Clay Art</em>
+                <span class="date">Sat, 24 October 2020</span>
+              </figcaption>
+            </figure>
+          </li>
+          <li class="list-item">
+            <figure>
+              <img src="./css/img/4.jpg" alt="1" width="160px">
+              <figcaption>
+                <em class="title">Britta Clay Art</em>
+                <span class="date">Sat, 24 October 2020</span>
+              </figcaption>
+            </figure>
+            */
