13fbc307e12f3ae1c4a111c729d0bc3121d4a9c1
ahnanne <{ahnanne93@gmail.com}>
2021-02-18 18:55:17 +0900

    feat: Complete app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,47 @@
+let currentSlide = 0;
+let slides = 0;
+
+// DOM content
+const $leftBtn = document.querySelector('.move-left_btn');
+const $rightBtn = document.querySelector('.move-right_btn');
+const $slide = document.querySelector('.carousel-slides');
+const $indicators = document.querySelector('.indicators');
+
 const carousel = ($container, images) => {
-  const $img = document.createElement('img');
+  slides = (images.length % 3 === 0) ? images.length / 3 : Math.floor(images.length / 3) + 1;
+
+  let html = '';
+
+  html = images.map(src => {
+    return `<li class="list-item">
+    <figure>
+      <img src=${src} alt="img" />
+      <figcaption>
+        <em class="title">Britta Clay Art</em>
+        <span class="date">Sat, 24 October 2020</span>
+      </figcaption>
+    </figure>
+  </li>`;
+  }).join('');
+
+  // images.map((src, idx) => {
+  //   if (!idx) 
+  //   if (idx === images.length - 1) 
+  // });
+
+  $container.innerHTML = html;
 };
 
-carousel(document.querySelector('.carousel'), [
+const indicator = ($indicators, slides) => {
+  let html = '';
+  const arr = new Array(slides).fill(0);
+
+  html = arr.map((_, idx) => `<button id=${idx} class="indicator"></button>`).join('');
+
+  $indicators.innerHTML = html;
+};
+
+carousel(document.querySelector('.carousel-list'), [
   './css/img/1.jpg',
   './css/img/2.jpg',
   './css/img/3.jpg',
@@ -12,35 +51,24 @@ carousel(document.querySelector('.carousel'), [
   './css/img/7.jpg',
   './css/img/8.jpg',
   './css/img/9.jpg',
-  './css/img/10.jpg',
-  './css/img/11.jpg',
 ]);
 
-/*
-<li class="list-item">
-            <figure>
-              <img src="./css/img/1.jpg" alt="1" width="160px">
-              <figcaption>
-                <em class="title">Britta Clay Art</em>
-                <span class="date">Sat, 24 October 2020</span>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="list-item">
-            <figure>
-              <img src="./css/img/2.jpg" alt="1" width="160px">
-              <figcaption>
-                <em class="title">Britta Clay Art</em>
-                <span class="date">Sat, 24 October 2020</span>
-              </figcaption>
-            </figure>
-          </li>
-          <li class="list-item">
-            <figure>
-              <img src="./css/img/4.jpg" alt="1" width="160px">
-              <figcaption>
-                <em class="title">Britta Clay Art</em>
-                <span class="date">Sat, 24 October 2020</span>
-              </figcaption>
-            </figure>
-            */
+indicator($indicators, slides);
+
+$rightBtn.onclick = () => {
+  if (currentSlide >= slides - 1) currentSlide = -1;
+  currentSlide += 1;
+  $slide.style.setProperty('--currentSlide', currentSlide);
+}
+
+$leftBtn.onclick = () => {
+  if (currentSlide <= 0) currentSlide = slides;
+  currentSlide -= 1;
+  $slide.style.setProperty('--currentSlide', currentSlide);
+}
+
+$indicators.onclick = e => {
+  if (!e.target.matches('button')) return;
+
+  $slide.style.setProperty('--currentSlide', e.target.id);
+};
